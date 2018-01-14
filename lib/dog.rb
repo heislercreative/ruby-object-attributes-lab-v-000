d19778c216c26ab29af21e8008312f7805fa0407
Brennan Heisler <learn@heislercreative.com>
2018-01-14 23:31:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,15 @@
 class Dog
-  def name=(input)
-    @name = input
+  def name=(name)
+    @name = name
   end
   def name
     @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
   end
 end
